161f783995157be650139af6914201515feb9357
ant1sg <antoine@sirven-gabiache.fr>
2024-08-14 20:08:11 +0200

    first draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,91 @@
-import { useState } from 'react'
 import UJLogo from './assets/LogoUJ_White.png'
-import AcidSerenade from './assets/AcidSerenades.png'
-import AudioPlayer from 'react-modern-audio-player';
+import AcidSerenade from './assets/UnionJack_AcidSerenades.jpg'
 import './App.css'
 import SocialGame from './assets/music/03.mp3';
 import Godspeed from './assets/music/01.mp3';
 import Echoes from './assets/music/02.mp3';
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import  { useState } from 'react';
 
-const playList = [
-  {
-    name: 'Social Game',
-    writer: 'UnionJack',
-    src: SocialGame,
-    id: 1,
 
-  },
-  {
-    name: 'Godspeed',
-    writer: 'UnionJack',
-    src: Godspeed,
-    id: 2,
-  },
-  {
-    name: 'Echoes',
-    writer: 'UnionJack',
-    src: Echoes,
-    id: 3,
-  },
+
+const playlist = [
+  { src: SocialGame, title: 'Social Game' },
+  { src: Godspeed, title: 'Godspeed' },
+  { src: Echoes, title: 'Echoes' },
 ]
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [currentTrack, setTrackIndex] = useState(0)
+  const handleClickNext = () => {
+      console.log('click next')
+        setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+    };
+  
+  const handleClickPrevious = () => {
+    console.log('click previous')
+      setTrackIndex((currentTrack) =>
+          currentTrack > 0 ? currentTrack - 1 : playlist.length - 1
+      );
+  };
+  
+  const handleEnd = () => {
+    console.log('end')
+    setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+  }
+  
   return (
     <div className="bg-white h-screen flex flex-col justify-center items-center p-4">
-      <div className='bg-blue-custom  border-2 border-black h-full w-full flex flex-col justify-start gap-8 pt-8 items-center bg-orange-image bg-cover'>
-        <div className="flex flex-row justify-center w-full">
-          <img src={UJLogo} className="animate-fadeInUp" alt="Union Jack" />
-          <img src={AcidSerenade} className="animate-fadeInUp" alt="Acid Serenade" />
-        </div>
-        <div className='w-4/5 drop-shadow-xl '>
-          <AudioPlayer 
-            playList={playList} 
-            audioInitialState={{
-              muted: false,
-              volume: 1,
-              curPlayId: 1,
-              repeatType: 'NONE',
-              isPlaying: true,
+      <div className='bg-blue-custom  border-2 border-black h-full w-full flex flex-row max-md:flex-col justify-start gap-8 pt-8 items-center bg-orange-image bg-no-repeat bg-right-bottom overflow-scroll'>
+        <div className='w-2/3 max-md:w-full flex flex-col justify-center items-center'>
+          <div className="flex flex-row flex-wrap justify-center w-full">
+            <img src={UJLogo} className="animate-fadeInUp" alt="Union Jack logo" />
+          </div>
+          <div className="text-white max-w-prose px-4 animate-fadeIn">
 
-              }}
-            placement={{
-              interface: {
-                templateArea: {
-                  progress: "row1-5",
-                  playButton: "row1-4",
-                  volume: "row1-8",
-                  playListButton: "row1-9",
-                },
-            
-              },
-              playList: "bottom",
-              player: "static",
-            }}
-            activeUI={{
-              progress: "waveform",
-              playList:"unSortable",
-              trackTime:false,
-              artwork: false,
-              volume: true,
-              trackInfo: true,
-              
-            }}
-            playListOpen={true}
-            /> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. 
+          </div>
+        </div>
+        <div className='w-1/3 max-md:w-full drop-shadow-xl flex flex-row justify-center animate-fadeInLeft'>
+        <AudioPlayer
+          src={playlist[currentTrack].src}
+          showJumpControls={false}
+          showSkipControls={true}
+          onPlay={e => console.log("onPlay")}
+          onClickNext={handleClickNext}
+          onClickPrevious={handleClickPrevious}
+          onEnded={handleEnd}
+          layout="stacked-reverse" 
+          customAdditionalControls={[]}
+          customVolumeControls={[]} 
+          header={
+            <div className='flex flex-col justify-center items-center gap-2'>
+              <img src={AcidSerenade} alt='Acid Serenade' className='w-1/2'/> 
+              <p className="flex flex-row justify-center"><strong>{playlist[currentTrack].title}</strong></p>
+            </div>
+        }
+          customControlsSection={
+            [ 
+              RHAP_UI.ADDITIONAL_CONTROLS,             
+              RHAP_UI.MAIN_CONTROLS,
+              RHAP_UI.VOLUME_CONTROLS,
+           ]}
+            style={{
+              width: 'clamp(300px, 75%, 1200px)',
+              borderRadius: '20px',
+              padding: '4em 2em',
+              backgroundColor: '#007db5',
+              color: 'white',
+              border: '4px solid #e75600',
+              boxShadow: '2px 2px 5px #000000',
+              margin: '2em auto',
+            }} 
+        />
         </div>
      
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,42 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        sm: ['0.8rem', '1rem'],
+        base: ['1rem', '1.5rem'],
+        lg: ['1.25rem', '1.75rem'],
+        xl: ['1.5rem', '2rem'],
+        '2xl': ['2rem', '2.5rem'],
+      },
       colors: {
         blue: {
-          custom: '#007cb4',
+          custom: '#007db5',
         },
       },
       keyframes: {
         fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '0%': { opacity: 0, transform: 'translateY(100px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInLeft: {
+          '0%': { opacity: 0, transform: 'translateX(100px)' },
+          '40%': { opacity: 0, transform: 'translateX(100px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0},
+          '50%': { opacity: 0},
+          '100%': { opacity: 1 },
         },
       },
       animation: {
         fadeInUp: 'fadeInUp 2s ease-in-out',
+        fadeInLeft: 'fadeInLeft 2s ease-in-out',
+        fadeIn: 'fadeIn 4s ease-in-out',
+        
       },
       backgroundImage: theme => ({
-        'orange-image': "url('assets/UnionJack_AcidSerenades_Oranges.jpg')",
+        'orange-image': "url('assets/UnionJack_AcidSerenades_Oranges_transparent2.png')",
       }),
     },
   },
