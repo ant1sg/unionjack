1fe7609b9e7bf76422dfc36ee1c4f95231f7e05e
ant1sg <antoine@sirven-gabiache.fr>
2024-08-14 15:13:03 +0200

    include audio player and sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,85 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UJLogo from './assets/LogoUJ_White.png'
+import AcidSerenade from './assets/AcidSerenades.png'
+import AudioPlayer from 'react-modern-audio-player';
 import './App.css'
+import SocialGame from './assets/music/03.mp3';
+import Godspeed from './assets/music/01.mp3';
+import Echoes from './assets/music/02.mp3';
+
+const playList = [
+  {
+    name: 'Social Game',
+    writer: 'UnionJack',
+    src: SocialGame,
+    id: 1,
+
+  },
+  {
+    name: 'Godspeed',
+    writer: 'UnionJack',
+    src: Godspeed,
+    id: 2,
+  },
+  {
+    name: 'Echoes',
+    writer: 'UnionJack',
+    src: Echoes,
+    id: 3,
+  },
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-white h-screen flex flex-col justify-center items-center p-4">
+      <div className='bg-blue-custom  border-2 border-black h-full w-full flex flex-col justify-start gap-8 pt-8 items-center bg-orange-image bg-cover'>
+        <div className="flex flex-row justify-center w-full">
+          <img src={UJLogo} className="animate-fadeInUp" alt="Union Jack" />
+          <img src={AcidSerenade} className="animate-fadeInUp" alt="Acid Serenade" />
+        </div>
+        <div className='w-4/5 drop-shadow-xl '>
+          <AudioPlayer 
+            playList={playList} 
+            audioInitialState={{
+              muted: false,
+              volume: 1,
+              curPlayId: 1,
+              repeatType: 'NONE',
+              isPlaying: true,
+
+              }}
+            placement={{
+              interface: {
+                templateArea: {
+                  progress: "row1-5",
+                  playButton: "row1-4",
+                  volume: "row1-8",
+                  playListButton: "row1-9",
+                },
+            
+              },
+              playList: "bottom",
+              player: "static",
+            }}
+            activeUI={{
+              progress: "waveform",
+              playList:"unSortable",
+              trackTime:false,
+              artwork: false,
+              volume: true,
+              trackInfo: true,
+              
+            }}
+            playListOpen={true}
+            /> 
+        </div>
+     
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          custom: '#007cb4',
+        },
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 2s ease-in-out',
+      },
+      backgroundImage: theme => ({
+        'orange-image': "url('assets/UnionJack_AcidSerenades_Oranges.jpg')",
+      }),
+    },
   },
   plugins: [],
 }
