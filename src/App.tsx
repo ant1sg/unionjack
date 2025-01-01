14448ab40371d0a30a448d802c0eeb1b9a654b28
ant1sg <antoine@sirven-gabiache.fr>
2025-01-01 20:27:10 +0100

    New style

--- src/App.tsx
+++ src/App.tsx
@@ -42,27 +42,27 @@ function App() {
   
   return (
     <div className="bg-white h-screen flex flex-col justify-center item-s-center p-4">
-      <div className='bg-blue-custom border-2 border-black h-full w-full flex flex-col max-md:flex-col justify-start bg-[url("./assets/FondBack-noborder.jpg")] bg-cover bg-no-repeat bg-right-bottom'>
+      <div className='bg-blue-custom border-2 border-black h-full w-full flex flex-col max-md:flex-col justify-start bg-[url("./assets/FondBack-noborder.jpg")] bg-cover bg-no-repeat bg-right-bottom overflow-scroll'>
       
-          <div className='h-full w-full bg-[length:50vw] bg-[url("./assets/orange2.png")]  bg-no-repeat bg-right-bottom  flex flex-row max-md:flex-col justify-start gap-8 pt-8 items-center '>
+          <div className='h-full w-full bg-[length:50vw] bg-[url("./assets/orange2.png")]  bg-no-repeat bg-right-bottom  flex flex-row max-lg:flex-col justify-start gap-8 lg:pt-8 items-center '>
 
-            <div className='w-1/3 max-md:w-full flex flex-col justify-center items-center'>
-              <div className="flex flex-row flex-wrap justify-end w-full max-md:hidden">
+            <div className='w-1/3 max-lg:w-full flex flex-col justify-center items-center'>
+              <div className="flex flex-row flex-wrap justify-end w-full max-lg:hidden">
                 <img src={UJLogo} className="animate-fadeInUp w-clamp-xl" alt="Union Jack logo" />
               </div>
             </div>
-            <div className='w-1/3 max-md:w-full flex flex-col justify-center items-center md:hidden'>
-              <div className=' flex flex-row justify-center flex-wrap justify-start w-full md:hidden'>
+            <div className='w-1/3 max-lg:w-1/2 max-md:w-full flex flex-col justify-center items-center lg:hidden'>
+              <div className=' flex flex-row justify-center flex-wrap justify-start w-full '>
                   <img src={AcidSerenadeTitle} alt='Acid Serenade' className='h-100 z-10'/>
-                  <img src={UJLogo} className="animate-fadeInUp -mt-40 w-1/2 " alt="Union Jack logo" />
+                  <img src={UJLogo} className="animate-fadeInUp -mt-40 w-1/2 max-lg:w-3/4" alt="Union Jack logo" />
                 
               </div>
             </div>
 
-          <div className='w-2/3 max-md:w-full drop-shadow-xl flex flex-col justify-center animate-fadeInLeft'>
-            <div className="text-white py-8 animate-fadeIn text-clamp-xl w-full flex flex-col justify-left items-left ">
-              <div className=' w-3/4 max-md:w-full'>
-                <div className='w-full flex flex-row justify-start  max-md:hidden'>
+          <div className='w-2/3 max-lg:w-full drop-shadow-xl flex flex-col justify-center animate-fadeInLeft'>
+            <div className="text-white lg:py-8 animate-fadeIn text-clamp-xl w-full flex flex-col justify-left items-left ">
+              <div className=' w-3/4 max-lg:w-full'>
+                <div className='w-full flex flex-row justify-start  max-lg:hidden'>
                   <img src={AcidSerenadeTitle} alt='Acid Serenade' className='h-100'/>
                 </div>
               <div className='text-clamp-xl p-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, debitis. Aperiam, accusamus nobis explicabo fugit odit ut obcaecati illo ducimus perspiciatis, autem, ratione facere laudantium aliquid libero sint nam iure. </p></div>
@@ -77,7 +77,7 @@ function App() {
 
       </div>
       
-      <div className='w-full p-4 gap-4 flex flex-row bg-white animate-fadeInUp h-[10vh]'>
+      <div className='w-full p-4 gap-4 flex flex-row bg-white animate-fadeInUp h-[20vh]'>
               <div className='w-1/4 p-1 h-full'>             
                 <img src={AcidSerenade} alt='Acid Serenade' className='w-full h-full object-contain'/> 
               </div>
